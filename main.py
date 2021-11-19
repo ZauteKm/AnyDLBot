2a26cdf0811b96794857fd77824c3bd36262c978
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2021-11-19 19:48:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -55,8 +55,8 @@ Hit help button to find out more about how to use me</b>""",
                                         InlineKeyboardButton('Feedback 👥', url='https://telegram.me/zautebot')
                                     ],[
                                         InlineKeyboardButton('🧑‍🔧 Owner', url='https://t.me/ZauteKm'),
-                                        InlineKeyboardButton('🤖 Bot Lists', url='https://t.me/BotzListBot'),
-                                        InlineKeyboardButton('Channel 📢', url='https://t.me/TGBotsProJect')
+                                        InlineKeyboardButton('🤖 Bot Lists', url='https://t.me/BotzList'),
+                                        InlineKeyboardButton('Channel 📢', url='https://t.me/JosProjects')
                                     ],[
                                         InlineKeyboardButton('🔻 Source Code -GitHub🔻', url='https://github.com/ZauteKm/AnyDLBot'),
                                     ]]
@@ -82,8 +82,8 @@ Just send a Youtube, Pornhub or Xhamster video url to download it in video or au
                                             "About 🙄", callback_data="about"),
                                   ],[
                                         InlineKeyboardButton("🧑‍🔧 Owner", url="https://t.me/ZauteKm"),
-                                        InlineKeyboardButton("🤖 Bot Lists", url="https://t.me/BotzListBot"),
-                                        InlineKeyboardButton('Channel 📢', url="https://t.me/TGBotsProJect")
+                                        InlineKeyboardButton("🤖 Bot Lists", url="https://t.me/BotzList"),
+                                        InlineKeyboardButton('Channel 📢', url="https://t.me/JosProjects")
                                     ],[
                                         InlineKeyboardButton("🔻 Source Code -GitHub🔻", url="https://github.com/ZauteKm/AnyDLBot"),
                                     ]]
@@ -112,9 +112,9 @@ async def about(client, message):
                                         InlineKeyboardButton(
                                             "Credit ❤️", url="https://t.me/ZauteBot"),
                                   ],[
-                                        InlineKeyboardButton("🧑‍🔧 Onwer", url="https://t.me/ZauteKm"),
-                                        InlineKeyboardButton("🤖 Bot Lists", url="https://t.me/BotzListBot"),
-                                        InlineKeyboardButton('Channel 📢', url="https://t.me/TGBotsProJect")
+                                        InlineKeyboardButton("🧑‍🔧 Owner", url="https://t.me/ZauteKm"),
+                                        InlineKeyboardButton("🤖 Bot Lists", url="https://t.me/BotzList"),
+                                        InlineKeyboardButton('Channel 📢', url="https://t.me/JosProjects")
                                     ],[
                                         InlineKeyboardButton("🔻 Source Code -GitHub 🔻", url="https://github.com/ZauteKm/AnyDLBot"),
                                     ]]
@@ -129,9 +129,41 @@ async def about(client, message):
                    & filters.text
                    & ~filters.edited
                    & filters.regex(YTDL_REGEX))
-async def ytdl_with_button(_, message: Message):
+async def ytdl_with_button(c: Client, message: Message):
+    if Config.UPDATES_CHANNEL is not None:
+        try:
+            user = await c.get_chat_member(Config.UPDATES_CHANNEL, message.chat.id)
+            if user.status == "kicked":
+                await c.send_message(
+                    chat_id=message.chat.id,
+                    text="Sorry, You are Banned to use me. Contact my [master](https://t.me/ZauteBot).",
+                    parse_mode="markdown",
+                    disable_web_page_preview=True
+                )
+                return
+        except UserNotParticipant:
+            await c.send_message(
+                chat_id=message.chat.id,
+                text="**Please Join My Updates Channel to use me 😉**",
+                reply_markup=InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton("Join Updates Channel", url=f"https://t.me/{Config.UPDATES_CHANNEL}")
+                        ]
+                    ]
+                ),
+                parse_mode="markdown"
+            )
+            return
+        except Exception:
+            await c.send_message(
+                chat_id=message.chat.id,
+                text="Something went Wrong. Contact my [master](https://t.me/zautebot).",
+                parse_mode="markdown",
+                disable_web_page_preview=True)
+            return
     await message.reply_text(
-        "**Choose download type👇**",
+        "**Choose Download type👇**",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
@@ -345,7 +377,7 @@ async def button(bot, update):
 print(
     """
 Bot Started!
-Join @TGBotsProJect
+Join @JosProjects
 """
 )
 
